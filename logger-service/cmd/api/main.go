154b80c474fbdfb678ff307cc973a968afdf21e7
Akansh Gupta <akansh.gupta1298@gmail.com>
2025-04-17 20:00:48 +0530

    Started logger service, added mongo db code, add mongo db in docker compose

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"logger-service/data"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Config struct {
+	Models data.Models
+}
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://localhost:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+func main() {
+
+	// connect to mongo db
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create a context in order to disconnect.
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+
+		}
+	}()
+
+	app := Config{
+		Models: data.New(mongoClient),
+	}
+	app.serve()
+
+}
+
+func (app *Config) serve() {
+	log.Println("starting service on port: ", webPort)
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic("unable to start the server")
+	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
+
+}
